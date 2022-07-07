9f06e9c8032abee7ef1cf1c47e9cbbb2006279c1
elishaeshoo7@gmail.com <elishaeshoo@Elishas-MacBook-Pro.local>
2022-07-06 18:48:41 -0700

    created some more functions

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+
 // prompt('Do you want to take care of Veemon?')
 //CREATE Class and parameters
 class Digimon {
@@ -19,7 +20,7 @@ class Monster extends Digimon {
         this.age = 1;
         this.sleepiness = 5;
     }
-    //Funciton to eat
+    //Function to eat
     feedPet() {
         this.hunger -= 2;
     }
@@ -40,45 +41,82 @@ const beginButton = document.querySelector('#Begin')
 const exerciseButton = document.querySelector('#Exercise!')
 const eatButton = document.querySelector('#Feed Monster')
 const lightButton = document.querySelector('#Lights On/Off')
-// eatButton.addEventListener('click', (setHunger))
 
 let theGame = {
 
-
+    //CREATE a function that will increase the hunger level through intervals
     setHunger: () => {
+        //Interval ID always equals the setInterval
         const intervalID = setInterval(() => {
-            const hungryID = document.querySelector('#Feed Monster');
-            hungryID.innerHTML = `Hunger: ${this.hunger}`;
-            this.hunger -= 3;
-
+            //DECLARE an ID and query select your ID
+            const hungryID = document.querySelector('#Hunger');
+            //Have the ID be the status bar
+            hungryID.innerHTML = `Hunger: ${petDigimon.hunger}`;
+            //Increase or decrease the value of chosen ID
+            petDigimon.hunger -= 3;
+            
+            //IF statement for death condition
             if(this.hunger <= 10) {
                 alert('You don\'t have what it takes to raise a Monster...');
             }
+            // FINALLY set the intervals by milliseconds
         }, 3000)
+            //ADD event listeners
             feedButton.addEventListener('click', () => {
                 petDigimon.feedPet();
                 const hungryID = document.querySelector('#Feed Monster');
-                hungryID.innerHTML = `Hunger: ${petDigimon.this.hunger}`
+                hungryID.innerHTML = `Hunger: ${petDigimon.hunger}`
                 // console.log(petDigimon.this.hunger)
             })
-    }
+    },
 
 
     letsPlay: () => {
         const intervalID = setInterval(() => {
-            const boredID = document.querySelector('#Exercise');
-            boredID.innerHTML = `Boredom: ${this.boredom}`;
-            this.boredom -= 2;
+            const exerciseID = document.querySelector('#Exercise');
+            exerciseID.innerHTML = `Boredom: ${petDigimon.boredom}`;
+            petDigimon.boredom -= 2;
 
-            if (this.boredom <= 10) {
+            if (petDigimon.boredom <= 10) {
                 alert('You don\'t have what it takes to raise a Monster...');
             }
         }, 4000)
         playButton.addEventListener('click', () => {
             petDigimon.playTime();
-            const boredID = document.querySelector('#Exercise');
-            boredID.innerHTML = `Boredom: ${this.boredom}`;
+            const exerciseID = document.querySelector('#Exercise');
+            exerciseID.innerHTML = `Boredom: ${petDigimon.boredom}`;
         })
+    },
+
+    evolveAge: () => {
+        const intervalID = setInterval(() =>{
+            const ageID = document.querySelector('#Age');
+            ageID.innerHTML = `Age: ${petDigimon.age}`;
+            petDigimon.age += 1
+
+            if(this.age <= 5) {
+                alert(`${petDigimon.name} is evolving!`);
+                document.querySelector(`#sprite`).src = `pngwing.com-2.png`;
+            }
+            if(this.age <= 15) {
+                alert(`${petDigimon.name} is evolving!`);
+                document.querySelector(`#sprite`).src = `pngwing.com-3.png`;
+            }
+        }, 10000)
+    },
+
+    monsterName: () => {
+        petDigimon.name = prompt('What is your monster\'s name?')
+        const nameID = document.querySelector(`.name`);
+        nameID.innerHTML = (`${petDigimon.name}`)
+    },
+
+    startGame: () => {
+        monsterName();
+        evolveAge();
+        letsPlay();
+        setHunger();
+
     }
 
-}
+}// END of object / game 
